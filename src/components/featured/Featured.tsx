37da67cf0cb8d724c4b47645f58341fd4b429dc6
caarlosdamian <dapecarlos@gmail.com>
2023-08-17 23:08:55 -0600

    feat(sections): init featured section

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+'use client';
+import { cardInfo } from '@/utils/info';
+import React from 'react';
 
 export const Featured = () => {
   return (
-    <div>Featured</div>
-  )
-}
+    <div>
+      {cardInfo?.map((card) => (
+        <div key={card.id}>
+          <div
+            className="w-screen h-screen bg-no-repeat"
+            style={{ backgroundImage: `url(${card.images[0]})` }}
+          ></div>
+          <div className="">
+            <h1>{card.title}</h1>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
---
+++ src/utils/info.tsx
@@ -0,0 +1,22 @@
+export const cardInfo = [
+    {
+      id: 1,
+      title: 'Enjoyable place for all the family',
+      desc: 'Our relaxed surroundings make dining with us a great experience for everyone. We can even arrange a tour of the farm before your meal.',
+      images: [
+        '/images/homepage/enjoyable-place-mobile@2x.jpg',
+        '/images/homepage/enjoyable-place-tablet@2x.jpg',
+        '/images/homepage/enjoyable-place-desktop@2x.jpg',
+      ],
+    },
+    {
+      id: 2,
+      title: 'The most locally sourced food',
+      desc: 'All our ingredients come directly from our farm or local fishery. So you can be sure that you’re eating the freshest, most sustainable food.',
+      images: [
+        '/images/homepage/locally-sourced-mobile@2x.jpg',
+        '/images/homepage/locally-sourced-tablet@2x.jpg',
+        '/images/homepage/locally-sourced-desktop@2x.jpg',
+      ],
+    },
+  ];
