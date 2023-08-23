081f61223f87f074e70821d88b0ff075a8ce060f
caarlosdamian <dapecarlos@gmail.com>
2023-08-22 19:17:24 -0600

    feat(sections): added footer section

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import Image from 'next/image';
+import React from 'react';
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="py-20 px-6 text-white bg-black flex flex-col items-center gap-11 md:flex-row md:items-start md:justify-start md:py-16 md:px-10 md:gap-32 lg:px-[165px] lg:gap-[190px]">
+      <div className="relative h-[40px] w-[103px]">
+        <Image src="/logo.svg" alt="logo" fill className="object-contain" />
+      </div>
+      <div className="flex flex-col text-center gap-8 text-sm font-normal tracking-[2px] uppercase md:text-start lg:flex-row lg:gap-32">
+        <div className="flex flex-col gap-3">
+          <p>Marthwaite, Sedbergh</p>
+          <p>Cumbria</p>
+          <p>+00 44 123 4567</p>
+        </div>
+        <div className="flex flex-col gap-3">
+          <p>OPEN TIMES</p>
+          <p>MON - FRI: 09:00 AM - 10:00 PM</p>
+          <p>SAT - SUN: 09:00 AM - 11:30 PM</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
